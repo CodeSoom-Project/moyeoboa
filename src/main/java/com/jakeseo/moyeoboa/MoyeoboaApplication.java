835d6ea60ee8094fabedfb13755a2c6bba76df3d
n00nietzsche <n00nietzsche@gmail.com>
2022-05-21 17:46:58 +0900

    first commit

---
+++ src/main/java/com/jakeseo/moyeoboa/MoyeoboaApplication.java
@@ -0,0 +1,13 @@
+package com.jakeseo.moyeoboa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyeoboaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoyeoboaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jakeseo/moyeoboa/MoyeoboaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakeseo.moyeoboa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoyeoboaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
