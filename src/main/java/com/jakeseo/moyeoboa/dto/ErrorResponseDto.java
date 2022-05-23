aef363fbf25eccd5243ff8847ed77f7c365cad88
n00nietzsche <n00nietzsche@gmail.com>
2022-05-23 23:45:15 +0900

    ErrorResponseDto 조금 더 구체화

--- src/main/java/com/jakeseo/moyeoboa/dto/ErrorResponseDto.java
+++ src/main/java/com/jakeseo/moyeoboa/dto/ErrorResponseDto.java
@@ -1,13 +1,18 @@
 package com.jakeseo.moyeoboa.dto;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class ErrorResponseDto {
+    private final HttpStatus status;
+    private final int statusCode;
     private final String exception;
     private final String message;
 
-    public ErrorResponseDto(String exception, String message) {
+    public ErrorResponseDto(HttpStatus status, String exception, String message) {
+        this.status = status;
+        this.statusCode = status.value();
         this.exception = exception;
         this.message = message;
     }
